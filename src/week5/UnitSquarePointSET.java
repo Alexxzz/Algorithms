df185eda6d34a4b7ebbb68b2f3c5d6707d7c0d76
Oleksandr Zahorskyi <AlexZ@Oleksandrs-MacBook-Pro.local>
2017-01-10 22:34:28 +0100

    Created infrastructure for the week 5 assignment

---
+++ src/week5/UnitSquarePointSET.java
@@ -0,0 +1,27 @@
+package week5;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public interface UnitSquarePointSET {
+    // is the set empty?
+    boolean isEmpty();
+
+    // number of points in the set
+    int size();
+
+    // add the point to the set (if it is not already in the set)
+    void insert(Point2D p);
+
+    // does the set contain point p?
+    boolean contains(Point2D p);
+
+    // draw all points to standard draw
+    void draw();
+
+    // all points that are inside the rectangle
+    Iterable<Point2D> range(RectHV rect);
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    Point2D nearest(Point2D p);
+}
---
+++ src/week5/UnitSquarePointSETTest.java
@@ -0,0 +1,13 @@
+package week5;
+
+import org.junit.Test;
+
+public class UnitSquarePointSETTest {
+    /**
+     * Corner cases
+     */
+    @Test
+    public void insert_null() {
+
+    }
+}
