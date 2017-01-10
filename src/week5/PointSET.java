fed5398edf44e1f9998024f180f4e114b0049b39
Oleksandr Zahorskyi <AlexZ@Oleksandrs-MacBook-Pro.local>
2017-01-11 00:23:02 +0100

    PointSET methods + tests

--- src/week5/PointSET.java
+++ src/week5/PointSET.java
@@ -1,46 +1,72 @@
 package week5;
 
-import edu.princeton.cs.algs4.Point2D;
-import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.*;
+
+import java.util.ArrayList;
 
 public class PointSET implements UnitSquarePointSET {
+    private final SET<Point2D> set;
+
     // construct an empty set of points
     public PointSET() {
-
+        set = new SET<>();
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return true;
+        return set.isEmpty();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return set.size();
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
-
+        if (p == null) throw new NullPointerException();
+        set.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return true;
+        if (p == null) throw new NullPointerException();
+        return set.contains(p);
     }
 
     // draw all points to standard draw
     public void draw()  {
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
 
+        for(Point2D p: set) {
+            StdDraw.point(p.x(), p.y());
+        }
     }
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect)  {
-        return null;
+        if (rect == null) throw new NullPointerException();
+        return () -> {
+            ArrayList<Point2D> pointsInRange = new ArrayList<>();
+            for (Point2D p: set) {
+                if (rect.contains(p)) pointsInRange.add(p);
+            }
+
+            return pointsInRange.iterator();
+        };
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return null;
+        if (p == null) throw new NullPointerException();
+        if (isEmpty()) return null;
+
+        MinPQ<Point2D> pq = new MinPQ<>(set.size(), p.distanceToOrder());
+        for (Point2D point: set) {
+            pq.insert(point);
+        }
+
+        return pq.min();
     }
 }
--- src/week5/UnitSquarePointSETTest.java
+++ src/week5/UnitSquarePointSETTest.java
@@ -1,14 +1,20 @@
 package week5;
 
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import week3.Point;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 import static com.sun.tools.internal.ws.wsdl.parser.Util.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(Parameterized.class)
 public class UnitSquarePointSETTest {
@@ -25,7 +31,7 @@ public class UnitSquarePointSETTest {
     public static Collection<Object[]> getParameters() {
         return Arrays.asList(new Object[][] {
                 {(UnitSquarePointSETFactory) PointSET::new},
-                {(UnitSquarePointSETFactory) KdTree::new}
+//                {(UnitSquarePointSETFactory) KdTree::new}
         });
     }
 
@@ -39,7 +45,134 @@ public class UnitSquarePointSETTest {
     public void insert_null() {
         try {
             sut.insert(null);
-            fail("Should throw NullPointerException when insert null");
+            fail("Should throw NullPointerException when insert parameter is null");
         } catch (NullPointerException ignored) { }
+    }
+
+    @Test
+    public void contains_null() {
+        try {
+            sut.contains(null);
+            fail("Should throw NullPointerException when contains parameter is null");
+        } catch (NullPointerException ignored) { }
+    }
+
+    @Test
+    public void range_null() {
+        try {
+            sut.range(null);
+            fail("Should throw NullPointerException when range parameter is null");
+        } catch (NullPointerException ignored) { }
+    }
+
+    @Test
+    public void nearest_null() {
+        try {
+            sut.nearest(null);
+            fail("Should throw NullPointerException when nearest parameter is null");
+        } catch (NullPointerException ignored) { }
+    }
+
+    /**
+     * isEmpty, size and contains on empty set
+     */
+    @Test
+    public void isEmpty_emptySet() {
+        assertTrue(sut.isEmpty());
+    }
+
+    @Test
+    public void size_emptySet() {
+        assertEquals(0, sut.size());
+    }
+
+    @Test
+    public void contains_emptySet() {
+        assertFalse(sut.contains(new Point2D(1, 2)));
+    }
+
+    @Test
+    public void nearest_emptySet() {
+        assertEquals(null, sut.nearest(new Point2D(1, 2)));
+    }
+
+    @Test
+    public void range_emptySet() {
+        int pointsCount = 0;
+        for (Point2D p: sut.range(new RectHV(1, 2, 3, 4))) {
+            pointsCount++;
+        }
+
+        assertEquals(0, pointsCount);
+    }
+
+    /**
+     * isEmpty, size and contains after insert
+     */
+    @Test
+    public void isEmpty_afterOneInsert() {
+        sut.insert(new Point2D(1, 2));
+
+        assertFalse(sut.isEmpty());
+    }
+
+    @Test
+    public void size_afterOneInsert() {
+        sut.insert(new Point2D(1, 2));
+
+        assertEquals(1, sut.size());
+    }
+
+    @Test
+    public void contains_afterOneInsert() {
+        Point2D p = new Point2D(1, 2);
+
+        sut.insert(p);
+
+        assertTrue(sut.contains(p));
+    }
+
+    /**
+     * Range
+     */
+    @Test
+    public void range_onePoint_inRange() {
+        Point2D p = new Point2D(0.5, 0.5);
+
+        sut.insert(p);
+
+        boolean pointInRange = false;
+        for (Point2D point: sut.range(new RectHV(0, 0, 1, 1))) {
+            pointInRange = point.equals(p);
+        }
+
+        assertTrue(pointInRange);
+    }
+
+    @Test
+    public void range_onePoint_outOfRange() {
+        Point2D p = new Point2D(0.5, 0.5);
+
+        sut.insert(p);
+
+        boolean pointInRange = false;
+        for (Point2D point: sut.range(new RectHV(0, 0, 0.1, 0.4))) {
+            pointInRange = point.equals(p);
+        }
+
+        assertFalse(pointInRange);
+    }
+
+    @Test
+    public void nearest_threePoints() {
+        Point2D p1 = new Point2D(0.5, 0.5);
+        Point2D p2 = new Point2D(0.6, 0.7);
+        Point2D p3 = new Point2D(0.8, 0.9);
+
+        sut.insert(p1);
+        sut.insert(p2);
+        sut.insert(p3);
+
+        assertEquals(p2, sut.nearest(new Point2D(0.6, 0.6)));
     }
 }
